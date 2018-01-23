be4146e34bb63def4deb12556ff61bb038cbb99a
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-23 12:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function takeANumber(currentLine, name){
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
+
+
+function nowServing(currentLine){
+  if(currentLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }else {
+    var string="The line is currently: ";
+    currentLine.forEach(function (element,index){
+      if(index != (currentLine.length-1)){
+        string = string + `${index +1}. ${currentLine[index]}, ` 
+      }else{
+        string = string + `${index +1}. ${currentLine[index]}`
+      }
+    })
+    currentLine = currentLine.shift();
+    
+  }
+}
